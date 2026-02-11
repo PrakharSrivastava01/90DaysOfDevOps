7c24e549b76f13149199e1f2164ec48386eaa581
Prakhar <Prakharsrivastava0106@gmail.com>
2026-02-11 17:29:49 +0530

    d-16 script completed

--- 2026/day-16/installation_packages.sh
+++ 2026/day-16/installation_packages.sh
@@ -1,2 +1,10 @@
-echo "hello"
+#!/bin/bash
 
+read -p " Enter the package, you want to install : " package
+echo " Installing the $package"
+
+sudo apt-get update &> /dev/null
+sudo apt-get install $package
+systemctl status $package
+
+echo "$package installed..!"
